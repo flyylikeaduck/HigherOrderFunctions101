ca55946132c58884283c8753b321ebdccccb3dfd
KevinG <kevin@kevin.gitignore>
2016-09-14 16:36:55 -0700

    Initial commit

---
+++ HOF101.js
@@ -0,0 +1,28 @@
+//YOUR CODE BELOW. HAVE FUN!
+var freethrow = function(n){
+	return n + 1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//======= DO NOT TOUCH loop FUNCTION =======
+var loop = function(collection, callback){
+	for (var i = 0; i < collecton.length; i++){
+		callback(collection[i]);
+	}
+}
+//======= DO NOT TOUCH loop FUNCTION =======
+//MORE OF YOUR CODE BELOW
